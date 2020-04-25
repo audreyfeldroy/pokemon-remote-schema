ccc8e79c6575e31e75d39ca963d09191c34ca3ef
Audrey Feldroy <audrey@feldroy.com>
2020-04-25 16:43:55 -0700

    Get the getPokemon() query working

--- index.js
+++ index.js
@@ -1,47 +1,30 @@
+const fetch = require('node-fetch');
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 
+const MY_REST_URL = 'https://pokeapi.co/api/v2'
+
 const typeDefs = gql`
-  type Query {
-    hello:  String
-  }
 
   type Pokemon {
     id:       String!
     name:     String!
-    baseExperience:  Int!
+    base_experience:  Int!
   }
 
   type Query {
     getPokemon(id: String!): Pokemon
-    pokemons(name: String): [Pokemon]
-  }
-
-  type Mutation {
-    addPokemon(name: String!, balance: Int!): Pokemon
   }
 `;
 
 const resolvers = {
     Query: {
-        hello: () => "world",
+        // hello: () => "world",
         getPokemon: async (_, { id }) => {
-            return await getData(MY_REST_URL + '/pokemon/' + id);
+            
+            const response = await fetch(MY_REST_URL + '/pokemon/' + id + '/');
+            return response.json();
         },
-
-        pokemon: async (_, { name }) => {
-            var nameParams = '';
-            if (name) {
-                nameParams = '?name=' + name;
-            }
-            return await getData(MY_REST_URL + '/pokemon' + nameParams);
-        }
-    },
-
-    Mutation: {
-        addPokemon: async (_, { name, balance } ) => {
-          return await postData(MY_REST_URL + 'pokemon', { name, balance } );
-        }
     }
 };
 
@@ -50,4 +33,3 @@ const schema = new ApolloServer({ typeDefs, resolvers });
 schema.listen({ port: process.env.PORT}).then(({ url }) => {
     console.log(`schema ready at ${url}`);
 });
-
